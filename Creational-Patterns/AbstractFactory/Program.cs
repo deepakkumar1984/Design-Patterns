2298f982adf7d0002b24575dea2946a7be068326
Deepak Kumar Battini <deepakkumar1984@gmail.com>
2015-12-27 12:37:46 +0530

    First comit with solution

---
+++ Creational-Patterns/AbstractFactory/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    //Provide an interface for creating families of related or dependent objects without specifying their concrete classes.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
